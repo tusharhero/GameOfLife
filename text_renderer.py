2b438a19025deaf8874112264d3e84bd2a66e71c
Tushar Maharanagit <tusharhero@sdf.org>
2023-03-27 23:31:14 +0530

    add text frontend for gol and other fixes for gol core

---
+++ text_renderer.py
@@ -0,0 +1,51 @@
+"""
+The GPLv3 License (GPLv3)
+
+Copyright (c) 2023 Tushar Maharana
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import gol_engine as gol
+import time
+alive = "⬜"
+dead = "⬛"
+
+canvas = gol.CreateCanvas((10, 10))
+
+for x_index in range(len(canvas)):
+    print(f" {x_index} ", end="")
+for y_index in range(len(canvas[0])):
+    print(f"\n{y_index}{canvas[y_index]}", end="")
+
+print("\nEnter the coordinates you want to spell life into:")
+
+while True:
+    coordinates = eval(input())
+    if not coordinates:
+        break
+    canvas[coordinates[0]][coordinates[1]] = 1
+
+for g in range(100):
+    print(chr(27) + "[2J") #escape codes to clear the terminal
+    print(f"generation:{g}")
+    for row in canvas:
+        for cell in row:
+            if cell == 1:
+                print(alive,end="")
+            else:
+                print(dead,end="")  
+        print()
+    canvas = gol.GetNextGen(canvas)
+    time.sleep(3)
